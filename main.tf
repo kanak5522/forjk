f796a5d4e6de7945fb8121ed40c717b6c921a14d
kanak5522 <kanakchandel552000@gmail.com>
2024-09-03 22:57:23 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,7 @@
 provider "aws" {
   region = "eu-west-1"
 }
-resource "aws_s3_bucket" "t12345_state" {
-  bucket = "nobuckk"
-  acl    = "private"
-}
 
-resource "aws_dynamodb_table" "ttlock" {
-  name           = "noolock"
-  hash_key        = "LockID"
-  read_capacity   = 1
-  write_capacity  = 1
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
 resource "aws_instance" "newinnn3t" {
     ami = "ami-04e49d62cf88738f1"
     instance_type = "t2.micro"
